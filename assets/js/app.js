1e1f1aecb4c8165ebfab595073006169bb722070
Andres Valdivieso <aavaldivieso@uc.cl>
2019-01-16 22:01:58 -0300

    galeria y etc

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,113 @@
-$(document).ready(function(){
-
-  $('.next').on('click', function(){
-    var currentImg = $('.active');
-    var nextImg = currentImg.next();
-
-    if(nextImg.length){
-      currenImg.removeClass('active').css('z-index, -10');
-      nextImg.addClass('active').css('z-index', 10);
-
-    }
-  });
-
+$(document).ready(function() {
   $('.prev').on('click', function(){
-    var currentImg = $('.active');
-    var prevImg = currentImg.prev();
+    var prevImg = $('img.active').prev('.slider-inner img');
+    if(prevImg.length == 0) {
+      prevImg = $('.slider-inner img:last');
+    }
+    $('img.active').removeClass('active');
+    prevImg.addClass('active');
+  });
+  $('.next').on('click', function() {
+    var nextImg = $('img.active').next('.slider-inner img');
+    if(nextImg.length == 0) {
+      nextImg = $('.slider-inner img:first');
+    }
+    $('img.active').removeClass('active');
+    nextImg.addClass('active');
+  });
 
-    if(prevImg.length){
-      currentImg.removeClass('active').css('z-index', -10);
-      nextImg.addClass('active').css('z-index', 10);
+let modalId = $('#image-gallery');
+
+$(document)
+  .ready(function () {
+
+    loadGallery(true, 'a.thumbnail');
+
+    //This function disables buttons when needed
+    function disableButtons(counter_max, counter_current) {
+      $('#show-previous-image, #show-next-image')
+        .show();
+      if (counter_max === counter_current) {
+        $('#show-next-image')
+          .hide();
+      } else if (counter_current === 1) {
+        $('#show-previous-image')
+          .hide();
+      }
+    }
+
+    /**
+     *
+     * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+     * @param setClickAttr  Sets the attribute for the click handler.
+     */
+
+    function loadGallery(setIDs, setClickAttr) {
+      let current_image,
+        selector,
+        counter = 0;
+
+      $('#show-next-image, #show-previous-image')
+        .click(function () {
+          if ($(this)
+            .attr('id') === 'show-previous-image') {
+            current_image--;
+          } else {
+            current_image++;
+          }
+
+          selector = $('[data-image-id="' + current_image + '"]');
+          updateGallery(selector);
+        });
+
+      function updateGallery(selector) {
+        let $sel = selector;
+        current_image = $sel.data('image-id');
+        $('#image-gallery-title')
+          .text($sel.data('title'));
+        $('#image-gallery-image')
+          .attr('src', $sel.data('image'));
+        disableButtons(counter, $sel.data('image-id'));
+      }
+
+      if (setIDs == true) {
+        $('[data-image-id]')
+          .each(function () {
+            counter++;
+            $(this)
+              .attr('data-image-id', counter);
+          });
+      }
+      $(setClickAttr)
+        .on('click', function () {
+          updateGallery($(this));
+        });
     }
   });
 
-  
+$(document)
+  .keydown(function (e) {
+    switch (e.which) {
+      case 37: // left
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-previous-image').is(":visible")) {
+          $('#show-previous-image')
+            .click();
+        }
+        break;
+
+      case 39: // right
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-next-image').is(":visible")) {
+          $('#show-next-image')
+            .click();
+        }
+        break;
+
+      default:
+        return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
+
 });﻿
+
 //append prepend click() evento.ready delegación de eventos
